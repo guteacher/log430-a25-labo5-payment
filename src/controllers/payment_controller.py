1204ac330af3b9dc9c9d921b9d4a7e6bcad144f0
Gabriel C. Ullmann <ullmanng162@gmail.com>
2025-12-27 18:29:04 -0500

    add logger and logger usage

--- src/controllers/payment_controller.py
+++ src/controllers/payment_controller.py
@@ -5,8 +5,11 @@ Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
 import numbers
 import requests
+from logger import Logger
 from commands.write_payment import create_payment, update_status_to_paid
 from queries.read_payment import get_payment_by_id
+
+logger = Logger.get_instance("payment")
 
 def get_payment(payment_id):
     return get_payment_by_id(payment_id)
@@ -25,23 +28,30 @@ def add_payment(request):
     
 def process_payment(payment_id, credit_card_data):
     """ Process payment with given ID, notify store_manager sytem that the order is paid """
-    # S'il s'agissait d'une véritable API de paiement, nous enverrions les données de la carte de crédit à un payment gateway (ex. Stripe, Paypal) pour effectuer le paiement. Cela pourrait se trouver dans un microservice distinct.
+    # S'il s'agissait d'un véritable service de paiement, nous utiliserions les données de la carte de crédit pour effectuer le paiement.
+    # Cela pourrait se trouver dans un microservice distinct.
     _process_credit_card_payment(credit_card_data)
 
     # Si le paiement est réussi, mettre à jour les statut de la commande
     # Ensuite, faire la mise à jour de la commande dans le Store Manager (en utilisant l'order_id)
     update_result = update_status_to_paid(payment_id)
-    print(f"Updated order {update_result['order_id']} to paid={update_result}")
+    logger.debug(f"Updated order {update_result['order_id']} to paid={update_result}")
     result = {
         "order_id": update_result["order_id"],
         "payment_id": update_result["payment_id"],
         "is_paid": update_result["is_paid"]
     }
+    # TODO: appelez la méthode correctement
+    update_order(0, False)
 
     return result
     
 def _process_credit_card_payment(payment_data):
     """ Placeholder method for simulated credit card payment """
-    print(payment_data.get('cardNumber'))
-    print(payment_data.get('cardCode'))
-    print(payment_data.get('expirationDate'))
+    logger.debug(payment_data.get('cardNumber'))
+    logger.debug(payment_data.get('cardCode'))
+    logger.debug(payment_data.get('expirationDate'))
+
+def update_order(order_id, is_paid):
+    """ Trigger order update once it is paid"""
+    pass
--- src/payments_api.py
+++ src/payments_api.py
@@ -3,10 +3,12 @@ Mock Payment Microservice using Flask
 SPDX - License - Identifier: LGPL - 3.0 - or -later
 Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
+from logger import Logger
 from flask import Flask, request, jsonify
 from controllers.payment_controller import add_payment, process_payment, get_payment
 
 app = Flask(__name__)
+logger = Logger.get_instance("payments")
 
 @app.route("/")
 def home():
@@ -16,18 +18,17 @@ def home():
 @app.route("/payments", methods=["POST"])
 def post_add_payment():
     """Create a new payment"""
-    print("Endpoint: POST /payments")
+    logger.debug("Endpoint: POST /payments")
     try:
         result = add_payment(request)
         return jsonify(result), 201
     except Exception as e:
-        print(e)
         return jsonify({"error": str(e)}), 400
 
 @app.route("/payments/process/<int:payment_id>", methods=["POST"])
 def post_process_payment(payment_id):
     """Process a simulated credit card payment"""
-    print(f"Endpoint: POST /payments/process/{payment_id}")
+    logger.debug(f"Endpoint: POST /payments/process/{payment_id}")
     try:
         credit_card_data = request.get_json() or {}
         result = process_payment(payment_id, credit_card_data)
@@ -47,7 +48,7 @@ def get_payment_details(payment_id):
 @app.errorhandler(404)
 def handle_404(error):
     """Handle 404 errors with JSON response"""
-    print(error)
+    logger.error(error)
     return jsonify({"error": "Endpoint ou ressource introuvable"}), 404
 
 # Start Flask app
